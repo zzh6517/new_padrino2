8cf9c1245f53e65db7ee86e5da73cb9fd7def0df
zhihao.zhang <zhihao.zhang@quaie.com>
2018-02-24 14:47:54 +0800

    更改消费者提供者名称

--- spec/pact_helper.rb
+++ spec/pact_helper.rb
@@ -3,13 +3,13 @@ require 'pact/provider/rspec'
 Pact.service_provider "new padrino2" do
 
   honours_pact_with 'new padrino1' do
-    pact_uri '../new_padrino1/spec/pacts/my_consumer-my_provider.json'
+    pact_uri '../new_padrino1/spec/pacts/new_padrino1-new_padrino2.json'
   end
 end
 
 
 
-Pact.provider_states_for "My Consumer" do
+Pact.provider_states_for "new padrino1" do
 
   provider_state "provider is in a sane state" do
     set_up do
